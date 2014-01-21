0a1a337531f0108332658286532909f33533d78d
Ryan J Dobrynski <ryjedo@gmail.com>
2014-01-20 20:41:23 -0700

    Refactors tempConversion into custom class and handles all numbers as NSDecimalNumbers. Still needs work to get rounding correct.

--- Metric Conversion/RJDtempConversion.m
+++ Metric Conversion/RJDtempConversion.m
@@ -8,14 +8,58 @@
 
 #import "RJDtempConversion.h"
 
-@interface RJDtempConversion ()
-
-
-@end
-
-
-
-
 @implementation RJDtempConversion
+
++ (NSString *)logger:(long)tempFcSelector withArg2:(long)tempPosNegSelector withArg3:(NSString *)tempUserInput
+{
+    
+    //declare numbers to be used for conversion process.
+    NSDecimalNumber *tempUnsignedNumber;
+    NSDecimalNumber *tempDoubleUnsignedNumber;
+    NSDecimalNumber *tempSignedNumber;
+    
+    //check for Positive or Negative number.
+    switch (tempPosNegSelector)
+    {
+        case 0:
+            tempUnsignedNumber = [[NSDecimalNumber alloc] initWithString:tempUserInput];
+            tempSignedNumber = tempUnsignedNumber;
+            break;
+            
+        case 1:
+            tempUnsignedNumber = [[NSDecimalNumber alloc] initWithString:tempUserInput];
+            tempDoubleUnsignedNumber = [[NSDecimalNumber alloc] decimalNumberByAdding:tempUnsignedNumber];
+            tempSignedNumber = [[NSDecimalNumber alloc] decimalNumberBySubtracting:tempDoubleUnsignedNumber];
+            break;
+    
+    }
+   
+    //declare NSObjects to be used for results
+    NSDecimalNumber *tempResultsNumber;
+    NSString *tempResultsString;
+    NSDecimalNumber *tempThirtyTwo = [[NSDecimalNumber alloc] initWithString:@"32.0"];
+    NSDecimalNumber *tempNine = [[NSDecimalNumber alloc] initWithString:@"9.0"];
+    NSDecimalNumber *tempFive = [[NSDecimalNumber alloc] initWithString:@"5.0"];
+    
+    //check for F or C selection.
+    switch (tempFcSelector)
+    {
+    
+        case 0:
+            tempResultsNumber = [[[[NSDecimalNumber alloc] decimalNumberBySubtracting:tempThirtyTwo] decimalNumberByMultiplyingBy:tempFive] decimalNumberByDividingBy:tempNine];
+            tempResultsString = [[[NSString alloc] initWithString:[tempResultsNumber description]] stringByAppendingString:@" C"];
+            break;
+
+        case 1:
+            tempResultsNumber = [[[[NSDecimalNumber alloc] decimalNumberByMultiplyingBy:tempNine] decimalNumberByDividingBy:tempFive] decimalNumberByAdding:tempThirtyTwo];
+            tempResultsString = [[[NSString alloc] initWithString:[tempResultsNumber description]] stringByAppendingString:@" F"];
+            break;
+    
+    }
+
+    return tempResultsString;
+
+}
+
 
 @end
--- Metric Conversion/RJDtempViewController.m
+++ Metric Conversion/RJDtempViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "RJDTempViewController.h"
-
+#import "RJDtempConversion.h"
 
 
 @interface RJDtempViewController ()
@@ -23,10 +23,7 @@
 @end
 
 
-
 @implementation RJDtempViewController
-
-
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -50,52 +47,28 @@
 }
 
 
-
-// converts user input to int, converts to other temp and pushes result to screen
 - (IBAction)RJDtempConvert:(id)sender
 {
-    //converts input to int.
-    if ([_RJDtempUserInput.text length] > 0) {
-        NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
-        NSNumber *tempUnsignedInt;
-        NSNumber *tempSignedInt;
-        switch (_RJDtempPosNegSelector.selectedSegmentIndex)
-        {
-            case 0:
-                tempUnsignedInt = [numberFormatter numberFromString:_RJDtempUserInput.text];
-                tempSignedInt = tempUnsignedInt;
-                break;
-                
-            case 1:
-                tempUnsignedInt = [numberFormatter numberFromString:_RJDtempUserInput.text];
-                tempSignedInt = @(tempUnsignedInt.intValue - (tempUnsignedInt.intValue * 2));
-                break;
-        }
-        
-        //calculates temp translation and pushes to screen
-        NSNumber *mathNumber;
-        switch (_RJDtempFcSelector.selectedSegmentIndex)
-        {
-            case 0:
-                mathNumber = @((tempSignedInt.intValue -32)* 5 / 9 );
-                _RJDtempResults.text = [[[NSString alloc] initWithString:[mathNumber description]] stringByAppendingString:@" C"];
-                break;
-    
-            case 1:
-                mathNumber = @(tempSignedInt.intValue * 9 / 5 + 32 );
-                _RJDtempResults.text = [[[NSString alloc] initWithString:[mathNumber description]] stringByAppendingString:@" F"];
-                break;
-        }
-    }else if ([_RJDtempUserInput.text length] == 0) {
-        _RJDtempResults.text = @"";
-    }
-}
 
+    if ([_RJDtempUserInput.text length] > 0) {
+        _RJDtempResults.text = [RJDtempConversion logger:_RJDtempFcSelector.selectedSegmentIndex withArg2:_RJDtempPosNegSelector.selectedSegmentIndex withArg3:_RJDtempUserInput.text];
+    
+    } else if ([_RJDtempUserInput.text length] == 0) {
+        _RJDtempResults.text = @"";
+    
+    } else {
+        NSLog(@"User Input with Neither Empty nor populated.. spooky.");
+    
+    }
+
+}
 
 
 - (IBAction)RJDtempEditClose:(id)sender
 {
+
     [self.view endEditing:YES];
+
 }
 
 
