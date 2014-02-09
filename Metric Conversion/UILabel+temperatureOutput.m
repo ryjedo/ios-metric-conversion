56e403b88cc0fe442360c88fda3f3795b3ed54cb
Ryan J Dobrynski <ryjedo@gmail.com>
2014-02-08 22:36:51 -0700

    Completes keyboard toolbar integration. changes interface to use labels for output instead of textfields.

---
+++ Metric Conversion/UILabel+temperatureOutput.m
@@ -0,0 +1,47 @@
+//
+//  UILabel+temperatureOutput.m
+//  Metric Conversion
+//
+//  Created by Ryan J Dobrynski on 2/8/14.
+//  Copyright (c) 2014 Ryan J Dobrynski. All rights reserved.
+//
+
+#import "UILabel+temperatureOutput.h"
+
+@implementation UILabel (temperatureOutput)
+
+
+
+
+
+// Fahrenheit
+- (void) setFahrenheitTemperatureFromDecimalNumber:(NSDecimalNumber *)displayTemperature
+{
+    self.text = [[[NSString alloc] initWithString:displayTemperature.description] stringByAppendingString:@"F"];
+}
+
+
+
+
+
+// Kelvin
+- (void) setKelvinTemperatureFromDecimalNumber:(NSDecimalNumber *)displayTemperature
+{
+    self.text = [[[NSString alloc] initWithString:displayTemperature.description] stringByAppendingString:@"K"];
+}
+
+
+
+
+
+// Celsius
+- (void) setCelsiusTemperatureFromDecimalNumber:(NSDecimalNumber *)displayTemperature
+{
+    self.text = [[[NSString alloc] initWithString:displayTemperature.description] stringByAppendingString:@"C"];
+}
+
+
+
+
+
+@end
